2a19677e47f32e0fde60f2a81270903dcc67dbfa
Matheus <matheus.frndes@gmail.com>
2015-09-28 11:21:09 -0300

    Fabric integration: Log in with Twitter

---
+++ app/src/main/java/com/tripgallery/BuildVars.java
@@ -0,0 +1,8 @@
+package com.tripgallery;
+
+/**
+ * Created by matheus on 9/28/15.
+ */
+public class BuildVars {
+    public static String LOG_TAG = "TP";
+}
--- app/src/main/java/com/tripgallery/MainActivity.java
+++ app/src/main/java/com/tripgallery/MainActivity.java
@@ -1,11 +1,10 @@
 package com.tripgallery;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -13,25 +12,43 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LoginButton loginButton;
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "cq5PCmd3snLt0xT7OHJ9YRREd";
+    private static final String TWITTER_SECRET = "72Fuoj06jzNGUWQCOBymHuplrRjhQUhsMJM4FHGRIcNAZAWei1";
+
+
+    private LoginButton facebookLoginButton;
+    private TwitterLoginButton twitterLoginButton;
     private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         FacebookSdk.sdkInitialize(getApplicationContext());
+
+        setContentView(R.layout.activity_main);
+
         callbackManager = CallbackManager.Factory.create();
 
-
-
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("public_profile,email,user_location");
+        facebookLoginButton = (LoginButton) findViewById(R.id.facebook_login_button);
+        facebookLoginButton.setReadPermissions("public_profile,email,user_location");
 
         // Callback registration
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        facebookLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
@@ -47,7 +64,19 @@ public class MainActivity extends AppCompatActivity {
                 // App code
             }
         });
-        setContentView(R.layout.activity_main);
+
+        twitterLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                Log.d(BuildVars.LOG_TAG, "twitter auth: success; @" + result.data.getUserName());
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(getApplicationContext(), ":(", Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
@@ -55,5 +84,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
     }
 }
