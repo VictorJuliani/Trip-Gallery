1af3d47d2e79a2689084eea844b4203fda3aeed6
Matheus <matheus.frndes@gmail.com>
2015-10-25 16:07:27 -0200

    Uploading photo and current location to Parse – Photo object

--- app/src/main/java/com/tripgallery/BuildVars.java
+++ app/src/main/java/com/tripgallery/BuildVars.java
@@ -4,7 +4,7 @@ package com.tripgallery;
  * Created by matheus on 9/28/15.
  */
 public class BuildVars {
-    public static String LOG_TAG = "TP";
+    public static String LOG_TAG = "TripGallery";
 
 	public static int LOCATION_INTERVAL = 5000;
 }
--- app/src/main/java/com/tripgallery/activity/MainActivity.java
+++ app/src/main/java/com/tripgallery/activity/MainActivity.java
@@ -1,16 +1,29 @@
 package com.tripgallery.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.util.Log;
+import android.widget.ImageView;
 
 import com.kbeanie.imagechooser.api.ChosenImage;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ProgressCallback;
+import com.parse.SaveCallback;
+import com.tripgallery.BuildVars;
 import com.tripgallery.R;
 import com.tripgallery.manager.LocManager;
+import com.tripgallery.manager.PreferenceManager_;
 import com.tripgallery.util.ImageSelectedCallback;
 import com.tripgallery.util.PhotoPicker;
 
@@ -18,6 +31,12 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.sharedpreferences.Pref;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity implements LocationListener, ImageSelectedCallback
@@ -25,16 +44,15 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
 	@ViewById
 	protected FloatingActionButton fab;
 
-	@ViewById(R.id.lbl_longitude)
-	protected TextView longitude;
-
-	@ViewById(R.id.lbl_latidude)
-	protected TextView latitude;
-
-	@ViewById
-	protected TextView provider;
+    @ViewById
+    protected ImageView imageView2;
 
 	private PhotoPicker picker;
+
+    @Pref
+    protected PreferenceManager_ preferences;
+
+    private Location currentLocation;
 
 	@AfterViews
 	protected void setup()
@@ -75,14 +93,55 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
 	@Override
 	public void handleImage(ChosenImage image)
 	{
-		// TODO
-	}
+        Bitmap bitMap = BitmapFactory.decodeFile(image.getFilePathOriginal());
+        imageView2.setImageBitmap(BitmapFactory.decodeFile(image.getFileThumbnailSmall()));
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        final ParseFile file;
+
+        bitMap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+        file = new ParseFile(uuid, stream.toByteArray());
+
+        file.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                ParseObject object = new ParseObject("Photo");
+                object.put("ownerId", preferences.userId().get());
+                object.put("file", file);
+                object.put("location", new ParseGeoPoint(currentLocation.getLatitude(),
+                                                         currentLocation.getLongitude()));
+                object.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Log.d(BuildVars.LOG_TAG, getResources().getString(R.string.photo_upload_success));
+                        Log.d(BuildVars.LOG_TAG, file.getUrl());
+                    }
+                });
+
+            }
+        }, new ProgressCallback() {
+            @Override
+            public void done(Integer integer) {
+                Log.d(BuildVars.LOG_TAG, integer.toString());
+            }
+        });
+
+    }
 
 	public void onLocationChanged(Location location)
 	{
-		this.provider.setText(location.getProvider());
-		longitude.setText(String.valueOf(location.getLongitude()));
-		latitude.setText(String.valueOf(location.getLatitude()));
+        this.currentLocation = location;
+		String cityName = null;
+		Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+		List<Address> addressList;
+		try {
+			addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+			if(addressList.size() > 0) {
+				Log.d("TG", addressList.get(0).getLocality());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void onStatusChanged(String provider, int status, Bundle extras)
