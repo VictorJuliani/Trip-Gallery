ff9041058ac956d3ef5e91a1061374b074ad0afc
marcusmourao <marcus-pf@hotmail.com>
2015-11-16 09:26:44 -0200

    Share on Facebook  (preview)

--- app/src/main/java/com/tripgallery/RecyclerViewAdapter.java
+++ app/src/main/java/com/tripgallery/RecyclerViewAdapter.java
@@ -3,14 +3,18 @@ package com.tripgallery;
 import android.animation.Animator;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
 import com.squareup.picasso.Picasso;
 import com.tripgallery.activity.ImageFullSizeActivity;
 
@@ -37,6 +41,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 		public TextView hashtagsView;
 		public Context context;
 		public ImageView full_size;
+		public Button shareButton;
 
 		public ViewHolder(View v)
 		{
@@ -45,6 +50,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 			cardView = (CardView) v.findViewById(R.id.cardView);
 			photoView = (ImageView) v.findViewById(R.id.photo);
 			hashtagsView = (TextView) v.findViewById(R.id.hashtags);
+			shareButton = (Button) v.findViewById(R.id.button);
 		}
 	}
 
@@ -70,16 +76,22 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
 		holder.hashtagsView.setText(post.hashtags);
 
-		holder.photoView.setOnClickListener(new View.OnClickListener()
-		{
+		holder.photoView.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View view)
-			{
+			public void onClick(View view) {
 				Intent intent = new Intent(holder.context, ImageFullSizeActivity.class);
 				intent.putExtra("url", post.url);
 				holder.context.startActivity(intent);
 
 				//zoomImageFromThumb(holder.photoView, post.url, holder.context, expandedImageView, holder.cardView);
+			}
+		});
+
+		holder.shareButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				shareOnFacebook(holder);
+
 			}
 		});
 	}
@@ -88,5 +100,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 	public int getItemCount()
 	{
 		return posts.size();
+	}
+
+	public void shareOnFacebook(final ViewHolder holder){
+
+		holder.photoView.buildDrawingCache();
+		Bitmap image = holder.photoView.getDrawingCache();
+		SharePhoto photo = new SharePhoto.Builder()
+				.setBitmap(image)
+				.build();
+		SharePhotoContent content = new SharePhotoContent.Builder()
+				.addPhoto(photo)
+				.build();
+
 	}
 }
